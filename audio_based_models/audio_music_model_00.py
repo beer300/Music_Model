f49a47602ab866f80c10fe3d25c1016806c4e901
beer300 <121382647+beer300@users.noreply.github.com>
2024-12-28 16:57:22 +0100

    ✨ Add Discriminator to audi_music_model

---
+++ audio_based_models/audio_music_model_00.py
@@ -0,0 +1,25 @@
+from torch import nn
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=1):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels, 16, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 1, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+
+        out = self.model(x)
+
+        return out.view(-1, 1)
+    
