c73e0d70b764d3920c63d438c06439a6fc010ec0
beer300 <121382647+beer300@users.noreply.github.com>
2025-02-07 21:02:03 +0000

    ✨ code data processer from begging

--- audio_based_models/dataset_processing.py
+++ audio_based_models/dataset_processing.py
@@ -1,102 +1,51 @@
-import os
 import torch
-from torch.utils.data import Dataset
 import torchaudio
-import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import os
+from torch_mdct import IMDCT, MDCT, vorbis
+
+mdct = MDCT(win_length=1024, window_fn=vorbis, window_kwargs=None, center=True)
+imdct = IMDCT(win_length=1024, window_fn=vorbis, window_kwargs=None, center=True)
 
 class AudioDataset(Dataset):
-    def __init__(self,
-                 audio_dir,
-                 transformation,
-                 target_sample_rate,
-                 num_samples,
-                 device):
+    def __init__(self, audio_dir, transform=None, target_sample_rate=48000):
         self.audio_dir = audio_dir
-        self.device = device
-        self.transformation = transformation.to(self.device)
-        self.target_sample_rate = target_sample_rate
-        self.num_samples = num_samples
         self.audio_files = [f for f in os.listdir(audio_dir) if f.endswith('.wav')]
-        
-        # Move resampler to the correct device
-        self.resampler = None
+        self.transform = transform
+        self.target_sample_rate = target_sample_rate
 
     def __len__(self):
         return len(self.audio_files)
 
-    def __getitem__(self, index):
-        audio_sample_path = os.path.join(self.audio_dir, self.audio_files[index])
-        signal, sr = torchaudio.load(audio_sample_path)
-        signal = signal.to(self.device)
-        signal = self._resample_if_necessary(signal, sr)
-        signal = self._mix_down_if_necessary(signal)
-        signal = self._cut_if_necessary(signal)
-        signal = self._right_pad_if_necessary(signal)
-        signal = self.transformation(signal)
-        signal = self._normalize(signal)  # Ensure values are between -1 and 1
-        return signal
+    def __getitem__(self, idx):
+        audio_path = os.path.join(self.audio_dir, self.audio_files[idx])
+        waveform, sample_rate = torchaudio.load(audio_path)
+        
+        if self.transform:
+            waveform = self.transform(waveform)
+        
+        
+        
+        if sample_rate != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=self.target_sample_rate)
+            waveform = resampler(waveform)
+        if waveform.size(1) < self.target_sample_rate:
+            padding = self.target_sample_rate - waveform.size(1)
+            waveform = torch.nn.functional.pad(waveform, (0, padding))
+        spectrogram = mdct(waveform)
+        return spectrogram
 
-    def _cut_if_necessary(self, signal):
-        if signal.shape[1] > self.num_samples:
-            signal = signal[:, :self.num_samples]
-        return signal
-
-    def _right_pad_if_necessary(self, signal):
-        length_signal = signal.shape[1]
-        if length_signal < self.num_samples:
-            num_missing_samples = self.num_samples - length_signal
-            last_dim_padding = (0, num_missing_samples)
-            signal = torch.nn.functional.pad(signal, last_dim_padding)
-        return signal
-
-    def _resample_if_necessary(self, signal, sr):
-        if sr != self.target_sample_rate:
-            if self.resampler is None:
-                self.resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate).to(self.device)
-            signal = signal.to(self.device)  # Move signal to same device as resampler
-            signal = self.resampler(signal)
-        return signal
-
-    def _mix_down_if_necessary(self, signal):
-        if signal.shape[0] > 1:
-            signal = torch.mean(signal, dim=0, keepdim=True)
-        return signal
-
-    def _normalize(self, signal):
-        signal_min = signal.min()
-        signal_max = signal.max()
-        signal = (signal - signal_min) / (signal_max - signal_min) * 2 - 1  # Scale to [-1, 1]
-        return signal
-
-
-if __name__ == "__main__":
-    AUDIO_DIR = r"C:\Users\Lukasz\Music\chopped_30"
-    SAMPLE_RATE = 48000
-    NUM_SAMPLES = 1440000
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using device {device}")
-
-    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-        sample_rate=SAMPLE_RATE,
-        n_fft=1024,
-        hop_length=512,
-        n_mels=64
-    )
-
-    # Add normalization transform
-    transform = nn.Sequential(
-        mel_spectrogram,
-        lambda x: (x - x.min()) / (x.max() - x.min()) * 2 - 1  # Scale to [-1, 1]
-    )
-
-    dataset = AudioDataset(
-        AUDIO_DIR,
-        transform,
-        SAMPLE_RATE,
-        NUM_SAMPLES,
-        device
-    )
+# Przykładowe użycie:
+audio_dir = r"C:\Users\lukas\Music\youtube_playlist_chopped"
+dataset = AudioDataset(audio_dir)
+data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
     
-    print(f"There are {len(dataset)} samples in the dataset.")
-    print(f"Sample shape: {dataset[0].shape}")
+# Iteracja przez batch'e
+for batch in data_loader:
+    spectrogram = batch
+    print(spectrogram.shape)
+    break
+
+# Konwersja pierwszego elementu z powrotem na plik audio
+#first_waveform, first_sample_rate = dataset[0]
+#torchaudio.save(r"C:\Users\lukas\Music\test\reconstructeed_audio.wav", first_waveform, first_sample_rate)
